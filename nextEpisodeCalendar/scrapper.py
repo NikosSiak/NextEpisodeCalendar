556aa1202bf939faf94ac4ae11a7c44e321f8aec
Nick Siakas <nikos.siakas@gmail.com>
2020-03-31 12:18:03 +0300

    Created scrapper class

---
+++ nextEpisodeCalendar/scrapper.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+from datetime import datetime
+import asyncio
+import aiohttp
+
+
+class SeriesScrapper:
+
+    def __init__(self, session: aiohttp.ClientSession):
+        self.now = datetime.now()
+        self.aiohttp_session = session
+    
+    async def findUrl(self, series_name: str):
+        series_name = series_name.replace(' ', '_')
+        url = f'https://en.wikipedia.org/wiki/List_of_{series_name}_episodes'
+        async with self.aiohttp_session.get(url) as resp:
+            if resp.status == 200:
+                return url
+        url = f'https://en.wikipedia.org/wiki/{series_name}_(TV_series)'
+        async with self.aiohttp_session.get(url) as resp:
+            if resp.status == 200:
+                return url
+    
+    
+   
